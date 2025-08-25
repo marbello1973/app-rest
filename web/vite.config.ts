9e39629055b0b9b26b1f79ea1ad2706a243a5623
marbello1973 <david.marbello@outlook.com>
2025-08-24 21:51:39 -0500

    mv pkg public

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,27 +1,17 @@
 import { defineConfig } from "vite";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+//import wasm from "vite-plugin-wasm";
+//import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@wasm": resolve(__dirname, "src/pkg/app_wasm"),
-    },
-  },
-  plugins: [wasm(), topLevelAwait()],
   server: {
     fs: {
       // Allow serving files from one level up to the project root
       allow: [".."],
     },
   },
-  optimizeDeps: {
-    exclude: ["@app-wasm/app-wasm"],
-  },
   build: {
     target: "esnext",
     outDir: "dist",
   },
   base: "/app-rest/",
-  assetsInclude: ["**/*.wasm"],
 });
